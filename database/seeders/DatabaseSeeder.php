bcf0eb87e833fbf1ad67b7bb923a1bf0656cf7f8
wenshiye <936818307@qq.com>
2022-11-02 16:00:23 +0000

    社交统计信息

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+        
         $this->call(UsersTableSeeder::class);
         $this->call(StatusesTableSeeder::class);
+        $this->call(FollowersTableSeeder::class);
+
         Model::reguard();
     }
 }
